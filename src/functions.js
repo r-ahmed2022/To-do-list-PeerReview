b420de39e5f900ad415ca13264ce12463e920ff9
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-07-22 21:40:31 +0530

    finished unit testing part-1

---
+++ src/functions.js
@@ -0,0 +1,56 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable linebreak-style */
+export class Tasks {
+     static tasklist = [];
+
+     static addTasks = (task) => {
+       const id = Tasks.tasklist.length + 1;
+       const desc = task;
+       const completed = false;
+       Tasks.tasklist.push({
+         index: id,
+         description: desc,
+         completed,
+       });
+       localStorage.setItem('tasks', JSON.stringify(Tasks.tasklist));
+     }
+
+     static deleteTask = (i) => {
+       Tasks.tasklist.splice(i, 1);
+       Tasks.tasklist.forEach((item) => {
+         if (item.index <= 0) {
+           item.index = 1;
+         } else {
+           item.index -= 1;
+         }
+       });
+       localStorage.setItem('tasks', JSON.stringify(Tasks.tasklist));
+     };
+}
+
+export class Show {
+ static showTasks = () => {
+   const storedList = JSON.parse(localStorage.getItem('tasks'));
+   const list = document.getElementById('to-do-list');
+   for (let i = 0; i < storedList.length; i += 1) {
+     Tasks.tasklist.push(
+       {
+         index: storedList[i].index,
+         description: storedList[i].description,
+         completed: storedList[i].completed,
+       },
+     );
+     const li = document.createElement('li');
+     li.setAttribute('class', 'list-item');
+     li.innerHTML += `<input type="checkbox" onchange="changed(${i}, '${Tasks.tasklist[i].description}')"  name="completed" class="completed">`;
+     li.innerHTML += `<div class="info"><span  class="task.info">${Tasks.tasklist[i].description}</span>`;
+     li.innerHTML += `<span class="task-info">${Tasks.tasklist[i].completed}</span></div>`;
+     li.innerHTML += `<div class="info"><button type="button" onclick="editTask(${i}, 
+                      '${Tasks.tasklist[i].description}')" class="action">Edit</button>`;
+     li.innerHTML += `<button type="button" onclick='Tasks.deleteTask(${i})' id="deletebtn" class="action">Del</button>
+                 </div>`;
+     list.append(li);
+   }
+ };
+}
---
+++ src/functions.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+import { Tasks, Show } from './functions.js';
+
+describe('add/remove functions', () => {
+  test('adds an element to the list', () => {
+    document.body.innerHTML = `
+    <div id="task" class="task-">
+      <ul id="to-do-list" class="to-do-list"></ul>
+    </div>
+`;
+    Tasks.addTasks();
+    Show.showTasks();
+    const list = document.querySelectorAll('.to-do-list');
+    expect(list).toHaveLength(1);
+  });
+  test('removes an element from the list', () => {
+    Tasks.deleteTask();
+    Show.showTasks();
+    const list = document.querySelectorAll('.to-do-list');
+    expect(list).toHaveLength(1);
+  });
+});
